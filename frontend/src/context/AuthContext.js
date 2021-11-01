817b69cbaf67f1f54b5c6a1294c1b96478e0e4ac
UsamaNawaz1 <usamanawaz1994@gmail.com>
2021-11-02 04:18:06 +0500

    authenticate using Access & Refresh Tokens

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,16 +1,18 @@
 import {createContext, useState, useEffect} from 'react'
 import jwt_decode from 'jwt-decode'
-
+import {useHistory} from 'react-router-dom'
 const AuthContext = createContext()
 
 export default AuthContext
-
+ 
 
 export const AuthProvider = ({children}) => {
 
-    let [user, setUser] = useState(null)
-    let [authTokens, setAuthTokens] = useState(null)
+    
 
+    let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null )
+    let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null )
+    const history = useHistory()
     let loginUser = async (e) => {
         e.preventDefault()
         
@@ -26,13 +28,23 @@ export const AuthProvider = ({children}) => {
     if (response.status == 200){
         setAuthTokens(data)
         setUser(jwt_decode(data.access))
+        localStorage.setItem('authTokens', JSON.stringify(data))
+        history.push('/')
     }else{
         alert('something went wrong!!')
     }
     }
+
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        history.push('/login')
+    }
     let contextData = {
         user:user,
-        loginUser: loginUser
+        loginUser: loginUser,
+        logoutUser:logoutUser, 
     }
     return (
         <AuthContext.Provider value={contextData}>
